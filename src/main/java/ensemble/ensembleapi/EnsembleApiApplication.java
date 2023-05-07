55be0add1662a4e16f4a3b8566c330e9040654b1
sieun <sieun960513@naver.com>
2023-05-07 15:17:10 +0900

    init

---
+++ src/main/java/ensemble/ensembleapi/EnsembleApiApplication.java
@@ -0,0 +1,13 @@
+package ensemble.ensembleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnsembleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnsembleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ensemble/ensembleapi/EnsembleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ensemble.ensembleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsembleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
